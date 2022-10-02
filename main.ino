815db1a58a08805788723bc6e25e2b87d214f832
Kerem Cömert <kerem0comert@gmail.com>
2022-10-02 11:23:41 +0200

    Implement smooth walking via bluetooth commands

--- main.ino
+++ main.ino
@@ -15,209 +15,113 @@
 //  --------                 --------
 //                Front
 
-#define I2CAdd 0x40 // Define the I2C address of PCA9685
-//#include <Servo.h>       //Call servo Library
-#include "HCPCA9685.h" //Call PCA9685 library
+#include <VarSpeedServo.h>
 #include <FlexiTimer2.h>
+#define I2CAdd 0x40 // Define the I2C address of PCA9685
+#include "HCPCA9685.h" //Call PCA9685 library
+#include <SoftwareSerial.h>
 HCPCA9685 HCPCA9685(I2CAdd);
+SoftwareSerial BTSerial (2,3);
 
-/*
-    Unsigned char is used rather than unsigned int for saving memory.
-    RELEVANT: Yes, interrupts occur, and are processed, during delay functions.
-    When the ISR finishes, and returns, the code that was executing resumes executing
-    where it left off. If a delay was in process, the delay will be resumed.
-    The delay will end at the same time it would have if the interrupt had not occurred
-*/
-typedef struct Vector3
-{
-  unsigned int x;
-  unsigned int y;
-  unsigned int z;
-} Vector3;
+const unsigned char SERVO_COUNT = 8;
+VarSpeedServo servos[SERVO_COUNT];
 
-typedef struct Joint
-{
-  volatile bool isMoving;
-  unsigned long moveTimeSoFar;
-  unsigned long targetTime;
-  volatile unsigned int currAngle;
-  unsigned int targetAngle;
-  unsigned char diff;
-} Joint;
-/*
-  enum MoveSpeeds {
-  TURN = 4,
-  LEG_MOVE = 8,
-  BODY_MOVE = 3,
-  STAND_SEAT = 1,
-  };
-*/
-const unsigned char JOINT_COUNT = 8;
-Joint joints[JOINT_COUNT];
-const unsigned int SERVO_MOVEMENT_TIME = 1000;
-const unsigned char SERVO_MOVEMENT_THERSHOLD = 10;
+const int SERVO_MIN_ANGLE = 0;
+const int SERVO_MAX_ANGLE = 180;
+const int HCPCA_MIN_ANGLE = 0;
+const int HCPCA_MAX_ANGLE = 480;
+const int SERVO_SPEED = 30;
 
+// The servos are connected to consecutive pins starting from Pin 9
+const unsigned int SERVO_PIN_OFFSET = 9; 
+const unsigned int HCPCA_SERVO_OFFSET = 2;
+int defaultAngles[8] = {90, 30, 60, 125, 90, 20, 50, 120};
 
 void setup()
 {
+  for (int i = 0; i < SERVO_COUNT; i++) {
+    servos[i].attach(SERVO_PIN_OFFSET + i);
+  }
   HCPCA9685.Init(SERVO_MODE);
   HCPCA9685.Sleep(false);
   Serial.begin(9600);
-  // Initialize joints
-  //int defaultAngles[8] = {150, 40, 140, 350, 250, 40, 160, 350};
-  int i;
-  for (i = 0; i < JOINT_COUNT; i++)
-  {
-    joints[i].targetAngle = 0;
-    joints[i].targetTime = 0;
-    joints[i].isMoving = false;
-    joints[i].moveTimeSoFar = 0;
-    joints[i].currAngle = 0; // defaultAngles[i];
-    joints[i].diff = 100;
-  }
-
-  FlexiTimer2::set(100, checkMovements);
+  BTSerial.begin(9600);
+  //initDefaultPositions();
+  FlexiTimer2::set(1, readValue);
   FlexiTimer2::start();
-
-  // Here, specify which move to start, e.g.:
-  /*
-    startTimedMovement(1, 350, 10000);
-    startTimedMovement(2, 350, 3000);
-    startTimedMovement(3, 100, 5000);
-    startTimedMovement(4, 100, 7000);
-    startTimedMovement(5, 350, 100000);
-  */
-  forward();
-
-}
-
-void startTimedMovement(unsigned char jointId, unsigned int targetAngle, unsigned long targetTime)
-{
-  joints[jointId].targetTime = targetTime;
-  joints[jointId].targetAngle = targetAngle;
-  joints[jointId].isMoving = true;
-  joints[jointId].moveTimeSoFar = 0;
 }
 
 void loop()
 {
+ handleBluetooth();
+  defaultPos();
+  delay(500);
+  /*
+  forward3();
+  forward3();
+  forward3();
+  */
+  
+  //initDefaultPositions();
 }
 
-void waitAllToReach()
-{
-  bool allReached = false;
-  while (!allReached)
-  {
-    allReached = true;
-    for (int i = 0; i < JOINT_COUNT; i++)
-    {
-      if (joints[i].isMoving)
-      {
-        // Serial.println("Servo " + String(i) + " is still moving."); 
-        allReached = false;
-        break;
-      }
-    }
-  }
-  noInterrupts();
-  Serial.println("All reached."); 
-
-  delay(50);
-  interrupts();
+void writeToHPCPA(unsigned char servoId, int angle) {
+  HCPCA9685.Servo(servoId + HCPCA_SERVO_OFFSET, angle);
 }
 
-void checkMovements(void)
+void readValue(void)
 {
   sei();
-  unsigned long currentTime = millis();
-  int i;
-  for (i = 0; i < JOINT_COUNT; i++)
-  {
-    if (joints[i].isMoving)
-    {
-      // Target time has been reached
-      if ((joints[i].moveTimeSoFar > joints[i].targetTime) || 
-          (joints[i].diff <= 3))
-          
-      {
-        joints[i].targetAngle = 0;
-        joints[i].targetTime = 0;
-        joints[i].isMoving = false;
-      }
-      else
-      { // Still need to move more
-        joints[i].currAngle = map(
-                                joints[i].moveTimeSoFar, // Current progress
-                                0,                       // Start time
-                                joints[i].targetTime,    // End time
-                                joints[i].currAngle,
-                                joints[i].targetAngle);
-        joints[i].moveTimeSoFar = currentTime;
-        Serial.println("Servo " + String(i) + " moves to angle " + String(joints[i].currAngle) + ". MoveTimeSoFar " + String(joints[i].moveTimeSoFar));
-        // delay(50);
-        HCPCA9685.Servo(i + 1, joints[i].currAngle);
-        joints[i].diff = abs(joints[i].currAngle - joints[i].targetAngle);
-        Serial.println(String(i) + "d" + String(joints[i].diff));
-      }
-    }
-    
+  for (int i = 0; i < SERVO_COUNT; i++) {
+    //Serial.println(String(i) + " moved to " + servos[i].read());
+    writeToHPCPA(i, map(servos[i].read(), SERVO_MIN_ANGLE, SERVO_MAX_ANGLE, HCPCA_MIN_ANGLE, HCPCA_MAX_ANGLE));
   }
-  //Serial.println("It took function " + String(millis() - currentTime) + " seconds to complete");
 }
 
-void forward()
-{
-  startTimedMovement(0, joints[0].currAngle + 100, SERVO_MOVEMENT_TIME);
-  startTimedMovement(1, joints[1].currAngle + 200, SERVO_MOVEMENT_TIME);
-  startTimedMovement(2, joints[2].currAngle + 300, SERVO_MOVEMENT_TIME);
-  startTimedMovement(3, joints[3].currAngle + 480, SERVO_MOVEMENT_TIME);
-
-  waitAllToReach();
-
-  startTimedMovement(0, 0, SERVO_MOVEMENT_TIME);
-  startTimedMovement(1, 0, SERVO_MOVEMENT_TIME);
-  startTimedMovement(2, 0, SERVO_MOVEMENT_TIME);
-  startTimedMovement(3, 0, SERVO_MOVEMENT_TIME);
+void defaultPos() {
+  //Serial.println("In forward2");
+  for (int i = 0; i < SERVO_COUNT; i++) {
+    servos[i].write(defaultAngles[i], SERVO_SPEED, false);
+  }
+  delay(1000);
 }
 
-/*
-  startTimedMovement(3, joints[3].currAngle - 200, 100000);
-    startTimedMovement(2, joints[2].currAngle - 200, 100000);
+void forward3() {
+  Serial.println("In forward3");
+  servos[3].write(defaultAngles[3] - 30, SERVO_SPEED, false);
+  servos[7].write(defaultAngles[7] - 30, SERVO_SPEED, false);
+  delay(500);
+  servos[0].write(defaultAngles[0] - 20, SERVO_SPEED, false);
+  servos[4].write(defaultAngles[4] + 20, SERVO_SPEED, false);
+  servos[2].write(defaultAngles[2] - 20, SERVO_SPEED, false);
+  servos[6].write(defaultAngles[6] + 20, SERVO_SPEED, false);
+  delay(500);
+  servos[3].write(defaultAngles[3] , SERVO_SPEED, false);
+  servos[7].write(defaultAngles[7] , SERVO_SPEED, false);
+  delay(500);
+  servos[1].write(defaultAngles[1] + 30 , SERVO_SPEED, false);
+  servos[5].write(defaultAngles[5] + 30 , SERVO_SPEED, false);
+  delay(500);
+  servos[0].write(defaultAngles[0] + 20, SERVO_SPEED, false);
+  servos[4].write(defaultAngles[4] - 20, SERVO_SPEED, false);
+  servos[2].write(defaultAngles[2] + 20, SERVO_SPEED, false);
+  servos[6].write(defaultAngles[6] - 20, SERVO_SPEED, false);
+  delay(500);
+  servos[1].write(defaultAngles[1] , SERVO_SPEED, false);
+  servos[5].write(defaultAngles[5] , SERVO_SPEED, false);
+  delay(500);
+}
 
-    delay(50);
-
-    startTimedMovement(3, joints[3].currAngle + 200, 10000);
-    startTimedMovement(2, joints[2].currAngle + 200, 10000);
-    startTimedMovement(4, joints[4].currAngle - 50, SERVO_MOVEMENT_TIME);
-    startTimedMovement(6, joints[6].currAngle - 50, SERVO_MOVEMENT_TIME);
-
-    delayMicroseconds(50);
-
-    startTimedMovement(0, joints[0].currAngle - 50, SERVO_MOVEMENT_TIME);
-    startTimedMovement(4, joints[4].currAngle + 50, SERVO_MOVEMENT_TIME);
-    startTimedMovement(2, joints[2].currAngle - 60, SERVO_MOVEMENT_TIME);
-    startTimedMovement(6, joints[6].currAngle + 60, SERVO_MOVEMENT_TIME);
-
-    delayMicroseconds(50);
-
-    startTimedMovement(3, joints[3].currAngle + 50, SERVO_MOVEMENT_TIME);
-    startTimedMovement(7, joints[7].currAngle + 50, SERVO_MOVEMENT_TIME);
-
-    delayMicroseconds(50);
-
-    startTimedMovement(1, joints[1].currAngle + 60, SERVO_MOVEMENT_TIME);
-    startTimedMovement(5, joints[5].currAngle + 60, SERVO_MOVEMENT_TIME);
-
-    delayMicroseconds(50); 
-
-    startTimedMovement(0, joints[0].currAngle + 50, SERVO_MOVEMENT_TIME);
-    startTimedMovement(4, joints[4].currAngle - 50, SERVO_MOVEMENT_TIME);
-    startTimedMovement(2, joints[2].currAngle + 60, SERVO_MOVEMENT_TIME);
-    startTimedMovement(6, joints[6].currAngle - 60, SERVO_MOVEMENT_TIME);
-    delayMicroseconds(50);
-
-    startTimedMovement(1, joints[1].currAngle - 60, SERVO_MOVEMENT_TIME);
-    startTimedMovement(5, joints[5].currAngle - 60, SERVO_MOVEMENT_TIME);
-    delayMicroseconds(50);
-*/
-
+void handleBluetooth(){
+  if(BTSerial.available()){
+    char bluetoothCommand = BTSerial.read();
+    switch(bluetoothCommand){
+      case 'F': forward3(); break;
+      case 'X': break;
+    }
+    Serial.write("Bluetooth command: " + bluetoothCommand);
+  }
+  if(Serial.available()){
+    BTSerial.write(Serial.read());
+  }
+}
